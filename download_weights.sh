89a4d6e0811e664a34439d7937ad8292c59587dd
hudsonmartins <hudsonbr95@gmail.com>
2026-04-22 17:10:17 -0300

    update download weights script

--- download_weights.sh
+++ download_weights.sh
@@ -2,4 +2,15 @@
 if [ ! -d "weights" ]; then
   mkdir weights
 fi
-wget https://github.com/cvg/LightGlue/releases/download/v0.1_arxiv/superpoint_lightglue.pth -P weights/
+
+echo "Downloading SuperPoint/LightGlue weights..."
+wget -O weights/superpoint_lightglue.pth https://github.com/cvg/LightGlue/releases/download/v0.1_arxiv/superpoint_lightglue.pth
+
+echo "Downloading Deep Homography ViT..."
+wget -O weights/deep_homography_vit.pth https://huggingface.co/hudsonmsb/deep_homography_vit/resolve/main/deep_homography_vit.pth
+
+echo "Downloading DDI-VO weights..."
+wget -O weights/ddi_vo.tar https://huggingface.co/hudsonmsb/DDI-VO/resolve/main/ddi_vo.tar
+
+echo
+echo "All weights downloaded successfully!"
